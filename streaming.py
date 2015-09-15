bccb9a281d7cfc914c3cdcade8111bddd88e94c0
aspera1631 <aspera1631@gmail.com>
2015-09-15 15:17:11 -0400

    added config file

--- streaming.py
+++ streaming.py
@@ -2,17 +2,36 @@
 from slistener import SListener
 import time, tweepy, sys
 from tweepy import OAuthHandler
+import ConfigParser
 
 #username = '' ## put a valid Twitter username here
 #password = '' ## put a valid Twitter password here
 #auth     = tweepy.auth.BasicAuthHandler(username, password)
 #api      = tweepy.API(auth)
 
+'''
+config = ConfigParser.RawConfigParser()
+config.read('../config/auth_config.cfg')
+
+# getfloat() raises an exception if the value is not a float
+# getint() and getboolean() also do this for their respective types
+consumer_key = config.get('SectionOne', 'consumer_key')
+consumer_secret = config.getint('SectionOne', 'consumer_secret')
+access_token = config.get('SectionOne', 'access_token')
+access_secret = config.getint('SectionOne', 'access_secret')
+'''
+
+from ConfigParser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('../config/config.ini')
+
+
 # Authentication settings
-consumer_key = 'xhCE3HtM6XYWmtlLlLzbtBCVf'
-consumer_secret = 'BilV44WczCaJsljmUY4vc79xzgIDcSoOaNgaZ3RM8LywUaP5xq'
-access_token = '494451363-KYSKQn5JgPvROwzr46jQJiUJJbD5NpLXzrL72X7M'
-access_secret = '2qjIS5qOR2U7f2duj6lw5lfBZZytx5hWqUfUSTc9mV4j6'
+consumer_key = parser.get('section1', 'consumer_key')
+consumer_secret = parser.get('section1', 'consumer_secret')
+access_token = parser.get('section1', 'access_token')
+access_secret = parser.get('section1', 'access_secret')
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
