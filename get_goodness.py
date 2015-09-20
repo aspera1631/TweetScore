new gradient function, 200k tweets. Fixed NaN problem.

get_goodness.py
@@ -18,22 +18,26 @@ def sql_to_df(database, table):
 
 
 # Import data from SQL
-df = sql_to_df('TweetScore', 'binned')
+df = sql_to_df('TweetScore', 'binned2')
 
+test_num = 50000
 
 # Code source: Jaques Grobler
 # License: BSD 3 clause
 
 # load targets
-data_Y = df["rt_log"].values
+data_Y = df["retweets"][:-test_num].values
+test_Y = df["retweets"][-test_num:].values
 
 # Load features
-data_X = df[["emo_num", "ht_num", "media_num", "txt_len_basic", "url_num", "user_num"]].values
+data_X = df[["emo_num", "ht_num", "media_num", "txt_len_basic", "url_num", "user_num"]][:-test_num].values
+test_X = df[["emo_num", "ht_num", "media_num", "txt_len_basic", "url_num", "user_num"]][-test_num:].values
 
 # Turn the linear features into polynomial features
-poly = PolynomialFeatures(3)
+poly = PolynomialFeatures(4)
 # Apply this transformation
 X_new = poly.fit_transform(data_X)
+X_new_test = poly.fit_transform(test_X)
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
@@ -45,10 +49,11 @@ regr.fit(X_new, data_Y)
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean square error
-print("Residual sum of squares: %.2f" % np.mean((regr.predict(X_new) - data_Y) ** 2))
+print("Residual sum of squares for training set: %.2f" % np.mean((regr.predict(X_new) - data_Y) ** 2))
+print("Residual sum of squares for test set: %.2f" % np.mean((regr.predict(X_new_test) - test_Y) ** 2))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(X_new, data_Y))
-
+print('Variance score for training set: %.2f' % regr.score(X_new, data_Y))
+print('Variance score for test set: %.2f' % regr.score(X_new_test, test_Y))
 # plots
 #x_axis = np.array(range(max(data_X)+2))[:, np.newaxis]
 #y_pred = regr.predict(poly.fit_transform(x_axis))
@@ -67,6 +72,8 @@ print('Variance score: %.2f' % regr.score(X_new, data_Y))
 
 # load all possible tweets <= 140 characters
 tweetspace = pd.read_pickle('legal_tweets_3')
+print tweetspace
+
 
 # calcuate the goodness column (-cost)
 goodness = []
@@ -98,5 +105,6 @@ tweetspace['desig'] = tweetspace.apply(lambda row: make_index(row), axis=1)
 ts = tweetspace.set_index("desig")
 
 #print len(ts2.index.values)
-ts.to_pickle("goodness_ind_3")
+ts.to_pickle("goodness_ord4")
 # ADD IN RE_INDEX_GOODNESS.PY
+

make_plots.py
@@ -29,27 +29,36 @@ def make_bins(max_lbl, step):
     return [bins, labels_out]
 
 # Load data
-df = pd.read_pickle('features_test')
+df = pd.read_pickle('binned_all')
 
 #df = sql_to_df("TweetScore", "twitter")
 
 ## Plot retweets vs basic text length
 
 # Take log of retweets
-df["rt_log"] = df["retweets"].apply(lambda tweet: np.log10(tweet + 1))
-
-df["fol_log"] = df["followers"].apply(lambda tweet: np.log10(tweet + 1))
+#df["rt_log"] = df["retweets"].apply(lambda tweet: np.log10(tweet + 1))
 
 
 
-# Bin the data by text length
-df_bins_txt = df.groupby(pd.cut(df["txt_len_total"], bins=15, labels=False)).mean()
+# plot ave retweets vs emoji
 
-#df_bins_emo = df.groupby(pd.cut(df["emo_num"], bins=20, labels=False)).mean()
+def plot_fits(feature):
+    sns.set_context("talk", font_scale=1)
+    ax = sns.regplot(x=feature, y="retweets", data=df, x_estimator=np.mean, fit_reg=True, x_ci=50, scatter=True, ci=None)
+    ax = sns.regplot(x=feature, y="retweets", data=df, fit_reg=True, order=2, x_ci=50, scatter=False, ci=None)
+    ax = sns.regplot(x=feature, y="retweets", data=df, fit_reg=True, order=3, x_ci=50, scatter=False, ci=None)
+    ax = sns.regplot(x=feature, y="retweets", data=df, fit_reg=True, order=4, x_ci=50, scatter=False, ci=None)
+    #ax = sns.regplot(x="emo_num", y="retweets", data=df, fit_reg=True)
+    ax.set(xlabel=feature, ylabel='Retweets')
+    plt.show()
 
-#df_emo_test = df.groupby(pd.cut(df["emo_num"], bins=20, labels=False))
+def plot_feature(feature):
+    ax = sns.regplot(x=feature, y="retweets", data=df, x_estimator=np.mean, fit_reg=False, x_ci=50, scatter=True)
+    ax.set(xlabel=feature, ylabel='Retweets')
+    plt.show()
 
-
+plot_feature("emo_num")
+plot_feature("ht_num")
 '''
 ## Plot log(RTs) vs length of text
 # Create bins and labels for follwoer counts
@@ -168,14 +177,6 @@ ax.set(xlabel='Number of user mentions', ylabel='log(Rewteets + 1)')
 plt.show()
 '''
 
-
-# Plot log(RTs) vs number of emoji
-sns.set_context("talk", font_scale=1)
-df.sort(columns="emo_num")
-ax = sns.lmplot(x="emo_num", y="rt_log", data=df, x_estimator=np.mean, fit_reg=False)
-ax.set(xlabel='Number of emoji', ylabel='log(Rewteets + 1)')
-#ax.set_yscale('log')
-plt.show()
 
 
 

rebin_dataframe.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 import MySQLdb
 
-def import_data():
+def import_data(sql_table):
     database = "TweetScore"
-    table = "twitter"
+    table = sql_table
     con = MySQLdb.connect(host='localhost', user='root', passwd='', db=database)
 
     df = pd.read_sql_query("select * from %s" % table, con, index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None)
@@ -95,7 +95,8 @@ def bin_data(df):
     feat_bins["media_num"] = pd.cut(df["media_num"], media_bins, labels=False)
     feat_bins["emo_num"] = pd.cut(df["emo_num"], emo_bins, labels=False)
     feat_bins["url_num"] = pd.cut(df["url_num"], url_bins, labels=False)
-    feat_bins["rt_log"] = df["retweets"].apply(lambda tweet: np.log10(tweet + 1))
+    feat_bins["retweets"] = df["retweets"]
+    #feat_bins["rt_log"] = df["retweets"].apply(lambda tweet: np.log10(tweet + 1))
 
     return feat_bins
 
@@ -115,13 +116,14 @@ def pickle_to_sql(filein, tableName, mode):
     return True
 
 
-df = import_data()
+df = import_data('cleaned')
+# doesn't need to be here, but it's a safeguard
 feat_bins = bin_data(df).dropna()
 con = MySQLdb.connect(host='localhost', user='root', passwd='', db='TweetScore')
 
 print len(df.index)
 print len(feat_bins.index)
-#feat_bins[90001:].to_sql(con=con, name="binned", if_exists="append", flavor='mysql')
+#feat_bins[180001:].to_sql(con=con, name="binned2", if_exists="append", flavor='mysql')
 
 
 # we're losing ~6k rows out of 172k to this NaN problem.

tweetscore.py
@@ -18,7 +18,6 @@ def replace_codes(text):
 # Counts the number of emoji in a tweet
 def emoji_txt(text):
     # search for retweet
-    #print text
     m = re.search('''text(.+?), u'\w''', text)
     if m:
         n = re.findall('(\\\U\w{8}|\\\u\w{4})', m.group(1))
@@ -29,6 +28,16 @@ def emoji_txt(text):
     else:
         return 0
 
+#def emoji_txt(text):
+#    # search for retweet
+#    #print text
+#    text1 = text.encode('unicode-escape')
+#    n = re.findall('(\\\U\w{8}|\\\u\w{4})', text1)
+#    if n:
+#        return len(n)
+#    else:
+#        return 0
+
 
 # Finds total length of emoji in a tweet
 def get_emo_len(number):
@@ -36,11 +45,17 @@ def get_emo_len(number):
 
 # Function to count the total length of all hashtags, etc
 def get_ent_len(entities):
+    # if the entity contains emoji, don't count them in the length. We already counted them.
     totlen = 0
     if len(entities) > 0:
         for item in entities:
-            indices = item.get("indices", [0,0])
-            len1 = indices[1] - indices[0]
+            text = item.get("text", "")
+            m = re.findall('(\\\U\w{8}|\\\u\w{4})', text.encode('unicode-escape'))
+            if m:
+                len1 = 0
+            else:
+                indices = item.get("indices", [0,0])
+                len1 = indices[1] - indices[0]
             totlen = totlen + len1
     return totlen
 
@@ -84,7 +99,7 @@ def clean_tweets(filein, fileout):
     tweets['ext_ent'] = map(lambda tweet: tweet.get("retweeted_status", tweet).get("entities", {}).get("extended_entities", []), tweets_data)
 
     # Count the hashtags, etc.
-    tweets['emo_num'] = map(lambda tweet: emoji_txt(str(tweet)), tweets_data)
+    tweets['emo_num'] = map(lambda tweet: emoji_txt(str(tweet).encode("unicode-escape")), tweets_data)
     tweets['ht_num'] = tweets['hashtags'].apply(len)
     tweets['user_num'] = tweets['users'].apply(len)
     tweets['url_num'] = tweets['urls'].apply(len)
@@ -102,9 +117,9 @@ def clean_tweets(filein, fileout):
     tweets['ext_len'] = tweets['ext_ent'].apply(get_ent_len)
 
     # Get the length of the text, and then subtract all of the entity lengths.
-    tweets['txt_len_total'] = tweets['text'].apply(len) - tweets['emo_num']  #accounts for double-counting of emoji chars
-    tweets['txt_len_basic'] = tweets['txt_len_total'] \
-                              - tweets[['ht_len','url_len','user_len','sym_len','media_len','ext_len','emo_num']].sum(axis=1)
+    tweets['txt_len_total'] = tweets['text'].apply(len)  #accounts for double-counting of emoji chars
+    tweets['txt_len_basic'] = tweets['txt_len_total'] - \
+                              tweets[['ht_len', 'user_len', 'sym_len', 'media_len', 'ext_len', 'emo_num']].sum(axis=1)
 
     # get the user ID and the number of followers of that user
     tweets['user_id'] = map(lambda tweet: tweet.get("retweeted_status", tweet).get("user", {}).get("screen_name"), tweets_data)
@@ -156,6 +171,8 @@ def count_https(list):
     return count
 
 def tweet_features(df, tweet):
+    # THIS VERSION IS DEPRECATED. USE VERSION FROM WEB APP
+
     # run a tweet through the parser
     p = ttp.Parser()
     result = p.parse(tweet)
@@ -190,36 +207,67 @@ def sql_to_df(database, table):
 
     return df
 
-pickle_to_sql('recommendations', 'recommendations', 'replace')
 
 
-#clean_tweets('data_1.json', 'features_1')
-#clean_tweets('data_2.json', 'features_2')
-#clean_tweets('data_3.json', 'features_3')
-#clean_tweets('data_4.json', 'features_4')
-#clean_tweets('data_5.json', 'features_5')
-#clean_tweets('data_6.json', 'features_6')
-#clean_tweets('data_7.json', 'features_7')
-#clean_tweets('data_8.json', 'features_8')
+#pickle_to_sql('recommendations', 'recommendations', 'replace')
+
+path = '../twitter data/raw data/'
+
+'''
+clean_tweets(path + 'data_1.json', 'features_1')
+clean_tweets(path + 'data_2.json', 'features_2')
+clean_tweets(path + 'data_3.json', 'features_3')
+clean_tweets(path + 'data_4.json', 'features_4')
+clean_tweets(path + 'data_5.json', 'features_5')
+clean_tweets(path + 'data_6.json', 'features_6')
+clean_tweets(path + 'data_7.json', 'features_7')
+clean_tweets(path + 'data_8.json', 'features_8')
+clean_tweets(path + 'data_9.json', 'features_9')
+clean_tweets(path + 'data_10.json', 'features_10')
+clean_tweets(path + 'data_11.json', 'features_11')
 
 #print df["emo_num"]
 
-#pickle_to_sql('features_1', 'twitter', 'append')
-#pickle_to_sql('features_2')
-#pickle_to_sql('features_3')
-#pickle_to_sql('features_4')
-#pickle_to_sql('features_5')
-#pickle_to_sql('features_6')
-#pickle_to_sql('features_7')
-#pickle_to_sql('features_8')
+pickle_to_sql('features_1', 'cleaned', 'append')
+pickle_to_sql('features_2', 'cleaned', 'append')
+pickle_to_sql('features_3', 'cleaned', 'append')
+pickle_to_sql('features_4', 'cleaned', 'append')
+pickle_to_sql('features_5', 'cleaned', 'append')
+pickle_to_sql('features_6', 'cleaned', 'append')
+pickle_to_sql('features_7', 'cleaned', 'append')
+pickle_to_sql('features_8', 'cleaned', 'append')
+pickle_to_sql('features_9', 'cleaned', 'append')
+pickle_to_sql('features_10', 'cleaned', 'append')
+pickle_to_sql('features_11', 'cleaned', 'append')
+'''
 
 #pickle_to_sql('binned_tweets', 'binned', 'replace')
 
 
 
-#df = sql_to_df("TweetScore", "twitter")
-#print df.head()
+# load the full data set from SQL, select unique tweets, save as pickle file.
+#df = sql_to_df("TweetScore", "cleaned")
+#tw_unique = df.groupby('tw_id').first()
+#tw_unique.to_pickle('features_all')
 
+
+# Run "rebin_dataframe" to convert full file to binned dataframe and then sql
+
+# load from SQL and write as pickle.
+#df = sql_to_df("TweetScore", "binned2")
+#df.to_pickle('binned_all')
+
+
+# plot the data to make sure it makes sense.
+
+
+# perform the fit with "get_goodness"
+
+# re-index with "re_index_goodness"
+
+# perform gradient calculation with "gradient"
+
+# Assemble recommendation list with "recommendations"
 
 
 # Fields are: